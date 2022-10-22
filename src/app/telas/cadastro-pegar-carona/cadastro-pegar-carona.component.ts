be65d898fc39f9600cdfc7f9241ace2fa5c6b537
Kaleo-Stark <kaleovieira.dev@gmail.com>
2022-10-22 10:01:13 -0300

    layout tela de cadastrro pegar carona

--- src/app/telas/cadastro-pegar-carona/cadastro-pegar-carona.component.ts
+++ src/app/telas/cadastro-pegar-carona/cadastro-pegar-carona.component.ts
@@ -7,8 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPegarCaronaComponent implements OnInit {
 
+  public ajuda = {
+    combustivel: {
+      ativo: false,
+      href: '../../../assets/icon/combustivel-inativo.png'
+    },
+    estacionamento: {
+      ativo: false,
+      href: '../../../assets/icon/estacionamento-inativo.png'
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {}
 
+  ativarIcone(tipoAjuda){
+    if(tipoAjuda === 'combustivel'){
+      this.ajuda.combustivel.ativo = !this.ajuda.combustivel.ativo;
+
+      if(this.ajuda.combustivel.ativo){
+        this.ajuda.combustivel.href = '../../../assets/icon/combustivel-ativo.png';
+      }else{
+        this.ajuda.combustivel.href = '../../../assets/icon/combustivel-inativo.png';
+      }
+    }else{
+      this.ajuda.estacionamento.ativo = !this.ajuda.estacionamento.ativo;
+
+      if(this.ajuda.estacionamento.ativo){
+        this.ajuda.estacionamento.href = '../../../assets/icon/estacionamento-ativo.png';
+      }else{
+        this.ajuda.estacionamento.href = '../../../assets/icon/estacionamento-inativo.png';
+      }
+    }
+  }
 }
